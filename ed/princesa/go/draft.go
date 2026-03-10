f395828860d06c2c12236374148b5179b2aa3514
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-03-10 16:56:55 -0300

    Explorando o tko e resolvendo o exercicio da princesa

---
+++ ed/princesa/go/draft.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var tamanho int
+    fmt.Scan(&tamanho)
+    var elemento int
+    fmt.Scan(&elemento)
+
+    for i := 0; i < tamanho; i++ {
+        
+    }
+
+}
