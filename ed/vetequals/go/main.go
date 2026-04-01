17b45326b6639faedb6fc032ca8985988396009d
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-04-01 14:51:16 -0300

    exercicio que usa recursão para verificar se dois vetores são iguais foi resolvida

---
+++ ed/vetequals/go/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func EhVazio(vet []int) bool {
+	return fmt.Sprint(vet) == "[]"
+}
+
+// não altere a assinatura
+func equals(a []int, b []int) bool {
+	ehVazio_a := EhVazio(a)
+	ehVazio_b := EhVazio(b)
+
+	if ehVazio_a != ehVazio_b {
+		return false
+	}
+
+	if ehVazio_a == true || ehVazio_b == true {
+		return true
+	}
+
+	if a[0] != b[0] {
+		return false
+	}
+
+	return equals(a[1:], b[1:])
+
+	// não use a função len para ver ou comparar os tamanhos
+	// utilize a função EhVazio para ver se o vetor é vazio
+	// você só pode consultar o primeiro elemento do vetor
+	// e não pode usar nenhum tipo de laço
+	// Use recursao para consultar os outros elementos equals(a[1:], b[1:])
+	// não altere o protótipo da função nem crie funções auxiliares
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	vet1 := str2slice(scanner.Text())
+	scanner.Scan()
+	vet2 := str2slice(scanner.Text())
+	if equals(vet1, vet2) {
+		fmt.Println("iguais")
+	} else {
+		fmt.Println("diferentes")
+	}
+}
+
+func str2slice(line string) []int {
+	parts := strings.Fields(line)
+	nums := make([]int, 0)
+	for i := 1; i < len(parts)-1; i++ {
+		value, _ := strconv.Atoi(parts[i])
+		nums = append(nums, value)
+	}
+	return nums
+}
