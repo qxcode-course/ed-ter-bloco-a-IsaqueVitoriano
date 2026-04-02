613f2507eead25da390a6d5af945b3d868e18b49
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-04-02 15:30:17 -0300

    estudando recursão e ainda resolvendo o exercicio treinando recursão com vetores

---
+++ ed/treinando/go/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func fazRecursao(vet []int) string{
+	if len(vet) == 1 {
+		return fmt.Sprintf("%d", vet[0])
+	}
+
+	transformaEmStr := fmt.Sprintf("%d, ", vet[0])
+	
+	return transformaEmStr + fazRecursao(vet[1:])
+}
+
+func tostr(vet []int) string {
+	if len(vet) == 0 {
+		return "[]"
+	}
+
+	return "[" + fazRecursao(vet) + "]"
+}
+
+func tostrrev(vet []int) string {
+	_ = vet
+	return ""
+}
+
+// reverse: inverte os elementos do slice
+func reverse(vet []int) {
+	tamanho_vetor := len(vet)
+
+	if tamanho_vetor <= 1 {
+		return
+	}
+
+	vet[0], vet[tamanho_vetor-1] = vet[tamanho_vetor-1], vet[0]
+
+	reverse(vet[1:tamanho_vetor-1])
+}
+
+// sum: soma dos elementos do slice
+func sum(vet []int) int {
+	if len(vet) == 0 {
+		return 0
+	}
+
+	return vet[0] + sum(vet[1:])
+}
+
+// mult: produto dos elementos do slice
+func mult(vet []int) int {
+	if len(vet) == 0 {
+		return 1
+	}
+
+	if len(vet) == 1 {
+		return vet[0]
+	}
+
+	return vet[0] * mult(vet[1:])
+}
+
+// min: retorna o índice e valor do menor valor
+// crie uma função recursiva interna do modelo
+// var rec func(v []int) (int, int)
+// para fazer uma recursão que retorna valor e índice
+func min(vet []int) int {
+	_ = vet
+	return 0
+}
+
+func main() {
+	var vet []int
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		args := strings.Fields(line)
+		fmt.Println("$" + line)
+
+		switch args[0] {
+		case "end":
+			return
+		case "read":
+			vet = nil
+			for _, arg := range args[1:] {
+				if val, err := strconv.Atoi(arg); err == nil {
+					vet = append(vet, val)
+				}
+			}
+		case "tostr":
+			fmt.Println(tostr(vet))
+		case "torev":
+			fmt.Println(tostrrev(vet))
+		case "reverse":
+			reverse(vet)
+		case "sum":
+			fmt.Println(sum(vet))
+		case "mult":
+			fmt.Println(mult(vet))
+		case "min":
+			fmt.Println(min(vet))
+		default:
+			fmt.Println("fail: comando invalido")
+		}
+	}
+}
