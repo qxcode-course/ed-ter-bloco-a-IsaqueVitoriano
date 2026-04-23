c2c14c464fd173402287509c75837945ed0f829e
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-04-23 17:02:52 -0300

    estou resolvendo a atividade multiset, fiz as funções Erase, erase, Count e Contains

--- ed/multiset/src/go/main.go
+++ ed/multiset/src/go/main.go
@@ -79,6 +79,77 @@ func (ms *MultiSet) expand() {
 	ms.capacity = newCapacity
 }
 
+func (ms *MultiSet) Contains(value int) bool {
+	for i := range ms.data {
+		if ms.data[i] == value {
+			return true
+		}
+	}
+	return false
+}
+
+func (ms *MultiSet) Erase(value int) error {
+	for i := 0; i < ms.size; i++ {
+		if ms.data[i] == value {
+			ms.erase(i)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("value not found")
+}
+
+func (ms *MultiSet) erase(index int) error {
+	for i := index; i < ms.size-1; i++ {
+		ms.data[i] = ms.data[i + 1]
+	}
+
+	ms.size -= 1
+
+	return nil
+}
+
+func (ms *MultiSet) Count(value int) int {
+	inicio := 0
+	fim := ms.size - 1
+
+	primeiraOcorrencia := -1
+
+	for inicio <= fim {
+		meio := (inicio + fim) / 2
+		if ms.data[meio] == value {
+			primeiraOcorrencia = meio
+			fim = meio - 1
+		} else if value > ms.data[meio] {
+			inicio = meio + 1
+		} else {
+			fim = meio - 1
+		}
+	}
+
+	if primeiraOcorrencia == -1 {
+		return 0
+	}
+
+	inicio = 0
+	fim = ms.size - 1
+
+	ultimaOcorrencia := -1
+	for inicio <= fim {
+		meio := (inicio + fim) / 2
+		if ms.data[meio] == value {
+			ultimaOcorrencia = meio
+			inicio = meio + 1
+		} else if value > ms.data[meio] {
+			inicio = meio + 1
+		} else {
+			fim = meio - 1
+		}
+	}
+
+	return (ultimaOcorrencia - primeiraOcorrencia) + 1
+}
+
 func Join(slice []int, sep string) string {
 	if len(slice) == 0 {
 		return ""
@@ -113,17 +184,28 @@ func main() {
 			ms = NewMultiSet(value)
 		case "insert":
 			for _, part := range args[1:] {
-			value, _ := strconv.Atoi(part)
-			ms.Insert(value)
+				value, _ := strconv.Atoi(part)
+				ms.Insert(value)
 			}
 		case "show":
 			fmt.Println(ms.String())
 		case "erase":
-			// value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			result := ms.Erase(value)
+			if result != nil {
+				fmt.Println(result)
+			}
 		case "contains":
-			// value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			if ms.Contains(value) {
+				fmt.Println("true")
+			} else {
+				fmt.Println("false")
+			}
 		case "count":
-			// value, _ := strconv.Atoi(args[1])
+			value, _ := strconv.Atoi(args[1])
+			ocorrencia := ms.Count(value)
+			fmt.Println(ocorrencia)
 		case "unique":
 		case "clear":
 		default:
