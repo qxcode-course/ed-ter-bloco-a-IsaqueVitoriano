5638a6ba2734383e5e5a6c5a79500d86090a8c84
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-04-28 17:02:32 -0300

    estive estudando lista encadeada, a atividade lista_d1 foi parcialmente concluida

--- ed/lista_d1/src/go/main.go
+++ ed/lista_d1/src/go/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
 type Node struct {
@@ -17,27 +18,96 @@ type List struct {
 	node *Node
 }
 
-func (l *List) NewLList() *List{
+func NewLList() *List{
 	n := &Node{info:0}
 	n.next = n
 	n.prev = n
 	return &List{node: n}
 }
 
-func Insert(B *Node) {
+func (l *List) Insert(B *Node) {
 	A := B.prev
 	C := B.next
 	A.next = B
 	C.prev = B
 }
 
-func Remove(B *Node) {
+func (l *List) Remove(B *Node) {
 	A := B.prev
 	C := B.next
 	A.next = C
 	C.prev = A
 	B.prev = nil
-	B.next = nil 
+	B.next = nil
+}
+
+func (l *List) PushFront(value int) {
+	node := &Node{info: value}
+
+	node.prev = l.node
+	node.next = l.node.next
+
+	l.Insert(node)
+}
+
+func (l *List) PushBack(value int) {
+	node := &Node{info: value}
+
+	node.prev = l.node.prev
+	node.next = l.node
+
+	l.Insert(node)
+}
+
+func (l *List) PopBack() {
+	if l.Size() > 0 {
+		l.Remove(l.node.prev)
+	}
+}
+
+func (l *List) PopFront() {
+	if l.Size() > 0 {
+		l.Remove(l.node.next)
+	}
+}
+
+func (l *List) Size() int {
+	contaElementos := 0
+
+	nodeAtual := l.node.next
+
+	for nodeAtual != l.node {
+		contaElementos++
+		nodeAtual = nodeAtual.next
+	}
+
+	return contaElementos
+}
+
+func (l *List) Clear() {
+	l.node.next = l.node
+	l.node.prev = l.node
+}
+
+func (l *List) String() string {
+	if l.Size() == 0 {
+		return "[]"
+	}
+
+	saida := "["
+
+	it := l.node.next
+
+	for it != l.node {
+		saida += fmt.Sprintf("%d", it.info)
+
+		if it.next != l.node {
+			saida += ", "
+		}
+		it = it.next
+	}
+
+	return saida + "]"
 }
 
 func main() {
@@ -63,23 +133,23 @@ func main() {
 		case "show":
 			fmt.Println(ll.String())
 		case "size":
-			// fmt.Println(ll.Size())
+			fmt.Println(ll.Size())
 		case "push_back":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	ll.PushBack(num)
-			// }
+			for _, v := range args[1:] {
+			num, _ := strconv.Atoi(v)
+				ll.PushBack(num)
+			}
 		case "push_front":
-			// for _, v := range args[1:] {
-			// 	num, _ := strconv.Atoi(v)
-			// 	ll.PushFront(num)
-			// }
+			for _, v := range args[1:] {
+			num, _ := strconv.Atoi(v)
+				ll.PushFront(num)
+			}
 		case "pop_back":
-			// ll.PopBack()
+			ll.PopBack()
 		case "pop_front":
-			// ll.PopFront()
+			ll.PopFront()
 		case "clear":
-			// ll.Clear()
+			ll.Clear()
 		case "end":
 			return
 		default:
