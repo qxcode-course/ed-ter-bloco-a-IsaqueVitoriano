1d2eb77b843db630b92167c2359fbf82b8d58372
Isaque Vitoriano <isaquevitoriano7@gmail.com>
2026-05-27 10:49:51 -0300

    a atividade fila_enc do bloco pilha foi resolvida

---
+++ ed/fila_enc/src/go/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+
+type Node[T any] struct {
+	Value T
+	next  *Node[T]
+}
+
+type Queue[T any] struct {
+	head *Node[T]
+	tail *Node[T]
+	size int
+}
+
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{}
+}
+
+func (q *Queue[T]) Enqueue(value T) {
+	node := &Node[T]{Value: value}
+
+	if q.head == nil {
+		q.head = node
+		q.tail = node
+		return
+	}
+
+	q.tail.next = node
+	q.tail = node
+}
+
+func (q *Queue[T]) Dequeue() (T, bool) {
+	if q.head == nil {
+		var zero T
+		return zero, false
+	}
+
+	valor := q.head.Value
+	q.head = q.head.next
+
+	return valor, true
+}
+
+func (q *Queue[T]) Peek() (T, bool) {
+	if q.head == nil {
+		var zero T
+		return zero, false
+	}
+
+	valor := q.head.Value
+
+	return valor, true
+}
+
+func (q *Queue[T]) Size() int {
+	return q.size
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	if q.Size() == 0 {
+		return true
+	}
+
+	return false
+}
+
+func (q *Queue[T]) Clear() {
+	q.size = 0
+}
+
+func (q *Queue[T]) String() string {
+	result := "["
+	for n := q.head; n != nil; n = n.next {
+		if n != q.head {
+			result += ", "
+		}
+		result += fmt.Sprintf("%v", n.Value)
+	}
+	return result + "]"
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	queue := NewQueue[int]()
+
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		fmt.Println("$" + line)
+		args := strings.Fields(line)
+		if len(args) == 0 {
+			continue
+		}
+		switch args[0] {
+		case "end":
+			break
+		case "show":
+			fmt.Println(queue)
+		case "push":
+			for _, arg := range args[1:] {
+			 	value, _ := strconv.Atoi(arg)
+			 	queue.Enqueue(value)
+			}
+		case "pop":
+			if _, ok := queue.Dequeue(); !ok {
+			 	fmt.Println("falha: fila vazia")
+			}
+		case "peek":
+			if value, ok := queue.Peek(); ok {
+			 	fmt.Println(value)
+			} else {
+			 	fmt.Println("falha: fila vazia")
+			}
+		default:
+			fmt.Println("Unknown command:", args[0])
+		}
+	}
+}
